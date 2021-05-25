fc7230456d2053e9fde6d0cd3cbd851af09b79af
Yavuz <yavuzgecikli@hotmail.com>
2021-05-25 14:08:57 -0400

    4.satir

--- src/gitReview/Ders1.java
+++ src/gitReview/Ders1.java
@@ -6,6 +6,7 @@ public class Ders1 {
 		System.out.println("Saat 17:52");
 		System.out.println("Saat 18:46");
 		System.out.println("Saat 19:17, Iskender Bey");
+		System.out.println("Saat 20:24, Hamza Bey");
 	}
 
 }
