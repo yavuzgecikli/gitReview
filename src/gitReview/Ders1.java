26fa0fb4f96da15ca7e21bb377fb963138499d79
yavuzgecikli <62161813+yavuzgecikli@users.noreply.github.com>
2021-05-25 13:40:24 -0400

    Update Ders1.java

--- src/gitReview/Ders1.java
+++ src/gitReview/Ders1.java
@@ -5,6 +5,7 @@ public class Ders1 {
 	public static void main(String[] args) {
 		System.out.println("Saat 17:52");
 		System.out.println("Saat 18:46");
+		System.out.println("Saat 19:17, Iskender Bey");
 	}
 
 }
