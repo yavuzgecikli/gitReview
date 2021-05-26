5fd4ecb7cc57feff3885b58690ded72d4f48808c
Yavuz <yavuzgecikli@hotmail.com>
2021-05-26 00:40:43 -0400

    feature 2. satir

--- src/gitReview/Ders1.java
+++ src/gitReview/Ders1.java
@@ -6,7 +6,7 @@ public class Ders1 {
 		System.out.println("Saat 17:52");
 		System.out.println("Saat 18:46");
 		System.out.println("Saat 19:17, Iskender Bey");
-		System.out.println("Saat 20:24, Hamza Bey, DEGISTIRDIM");
+		System.out.println("Saat 20:50, Hamza Bey CONFLICT");
+		System.out.println("Saat 18:01 - feature branch 1. satir"); 
 	}
-
 }
